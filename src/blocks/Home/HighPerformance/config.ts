5265c03008e1408f17590036b79099b7bf822f11
vijay <vijay.tecneural@gmail.com>
2026-03-21 18:34:14 +0530

    s3 bucket added

--- src/blocks/Home/HighPerformance/config.ts
+++ src/blocks/Home/HighPerformance/config.ts
@@ -16,16 +16,19 @@ export const HighPerformance: Block = {
     {
       name: 'title',
       type: 'text',
+      defaultValue: "Acceoerating high-performance and sustainable AI",
       required: true,
     },
     {
       name: 'description',
       type: 'textarea',
+      defaultValue: "We design and engineer the physical infrastructure powering AI data centers from high-density cooling systems to advanced power systems enabling faster, greener, and more resilient computing.",
       required: true,
     },
     {
       name: 'buttonLabel',
       type: 'text',
+      defaultValue: "View more"
     },
     {
       name: 'buttonLink',
