b8794f30749074bf5d5998141f67c96c448069c4
tecneural <tecneural24@gmail.com>
2026-04-15 15:47:48 +0530

    preview issues

--- src/lib/getHomePage.ts
+++ src/lib/getHomePage.ts
@@ -4,32 +4,35 @@ import config from '@/payload.config'
 import { unstable_cache } from 'next/cache'
 import { draftMode } from 'next/headers'
 
-const { isEnabled : draft} = await draftMode()
-
-const getCachedHomePage = unstable_cache(
-  async () => {
-    const payload = await getPayload({ config })
-    const res = await payload.find({
-      collection: 'pages',
-      draft: draft,
-      limit: 1,
-      pagination: false,
-      where: {
-        or: [
-          { slug: { equals: 'home' } },
-          { slug: { equals: '/home' } },
-          { slug: { equals: '/' } },
-        ],
-      },
-    })
-    return res.docs[0] || null
-  },
-  ['home-page'],
-  { tags: ['home-page', 'pages'], revalidate: 60 },
-)
-
 export const getHomePage = async () => {
   try {
+    const { isEnabled: draft } = await draftMode()
+
+    const getCachedHomePage = unstable_cache(
+      async () => {
+        const payload = await getPayload({ config })
+
+        const res = await payload.find({
+          collection: 'pages',
+          draft,
+          limit: 1,
+          pagination: false,
+          overrideAccess: draft,
+          where: {
+            or: [
+              { slug: { equals: 'home' } },
+              { slug: { equals: '/home' } },
+              { slug: { equals: '/' } },
+            ],
+          },
+        })
+
+        return res.docs[0] || null
+      },
+      ['home-page'],
+      { tags: ['home-page', 'pages'], revalidate: 60 }
+    )
+
     return await getCachedHomePage()
   } catch (error) {
     console.error('Error fetching home page:', error)
